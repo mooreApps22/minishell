42c37b2bf1de4cda5a9719a800fe2c1ba305bdd2
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2024-09-12 18:34:19 +0100

    Update get_prompt.c

--- src/get_prompt.c
+++ src/get_prompt.c
@@ -1,14 +1,24 @@
 #include "../include/minishell.h"
 
+//smoore@c2r6s6:~$
+
 char	*get_prompt(void);
 
 char	*get_prompt(void)
 {
+	char	*prompt;
+	char	*temp;
 	char	*user;
+	char	*address;
 
 	user = getenv("USER");
-	if (!user)
-		printf("User not found\n");
-	user = ft_strjoin(user, "@");
-	return(user);
+	address = getenv("SESSION_MANAGER");
+	address = ft_substr(address, 6, 6);
+	prompt = ft_strjoin(user, "@");
+	temp = ft_strjoin(prompt, address);
+	free(prompt);
+	free(address);
+	prompt = ft_strjoin(temp, ":~$");
+	free(temp);
+	return (prompt);
 }
